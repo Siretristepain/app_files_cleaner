544d023430e9868e3035e3aa9ec489087cc48aa5
Raphael Ollier <raphael.ollier7@gmail.com>
2024-06-20 16:22:02 +0200

    Ajout docstring fonction main (Raphael).

--- cleaner.py
+++ cleaner.py
@@ -7,6 +7,21 @@ def main(extension: str = typer.Argument(".txt", help="Extension des fichiers à
         delete: bool = typer.Option(False, help="Option de suppression des fichiers trouvés"),
         recursive: bool = typer.Option(False, help="Option de recherche dans les sous-répertoires")):
     
+    """Fonction principale de l'application.
+    Elle fait la liste des fichiers trouvés avec l'extension donnée dans le répertoire donné (et ses sous-répertoire au choix).
+    Elle permet également de supprimer ces fichiers dans le cas où l'option --delete est saisie par l'utilisateur.
+    Dans ce cas, une demande de confirmation est envoyée à l'utilisateur.
+
+    Args:\n
+        extension (str, optional): Extension de fichier à rechercher. Par défaut sur ".txt". \n
+        path (str, optional): Chemin vers le répertoire où faire la recherche. Par défaut, il s'agit du répertoire de travail.\n
+        delete (bool, optional): Option de suppression des fichiers. Par défaut sur "False" (-> --no-delete).\n
+        recursive (bool, optional): Option de recherche récursive dans les sous-répertoires. Par défaut sur "False" (-> --no-recursive).\n
+
+    Raises:\n
+        typer.Abort: Arrêt du processus dans le cas où la confirmation de suppression des fichiers est négative.
+    """
+    
     typer.echo(f"Extension : {extension}\nDelete : {delete}\nPath: {path}\nRecursive : {recursive}")
 
     # Si l'utilisateur n'a pas précéde l'extension d'un '.', on l'ajoute
@@ -29,7 +44,7 @@ def main(extension: str = typer.Argument(".txt", help="Extension des fichiers à
         for file in all_files:
             typer.secho(file, fg=typer.colors.BLUE)
     else:
-        typer.echo(f"Aucun fichier trouvés avec l'extension {extension}")
+        typer.secho(f"Aucun fichier trouvés avec l'extension {extension}", bold=True, bg=typer.colors.GREEN)
 
     # STRUCTURE pour demander la confirmation à l'utilisateur
     if delete:
